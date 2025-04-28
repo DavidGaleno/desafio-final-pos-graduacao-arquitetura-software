32751ad6a040e82801b4e34d34b664c7b6db8c05
david galeno <davidgaleno411@gmail.com>
2025-04-28 00:18:11 -0300

    feat docker image and fix minDateValidator

--- src/main/java/galeno/david/validators/MinDateValidator.java
+++ src/main/java/galeno/david/validators/MinDateValidator.java
@@ -4,12 +4,9 @@ import galeno.david.annotations.MinDate;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
-import java.time.Instant;
 import java.time.LocalDate;
-import java.time.ZoneId;
-import java.util.Date;
 
-public class MinDateValidator implements ConstraintValidator<MinDate, Date> {
+public class MinDateValidator implements ConstraintValidator<MinDate, LocalDate> {
 
     private LocalDate minDate;
 
@@ -19,15 +16,11 @@ public class MinDateValidator implements ConstraintValidator<MinDate, Date> {
     }
 
     @Override
-    public boolean isValid(Date value, ConstraintValidatorContext context) {
-        if (value == null) {
+    public boolean isValid(LocalDate localDate, ConstraintValidatorContext context) {
+        if (localDate == null) {
             return true;
         }
 
-        LocalDate valueLocalDate = Instant.ofEpochMilli(value.getTime())
-                .atZone(ZoneId.systemDefault())
-                .toLocalDate();
-
-        return !valueLocalDate.isBefore(minDate);
+        return !localDate.isBefore(minDate);
     }
 }
